db48d4c3e093ce84aaeb05926719c897629fc6b6
Shoumen Das <shoumen.out.source@gmail.com>
2019-09-27 23:51:32 +0600

    DB Context create & Modified App settings

--- Backend/Backend/Startup.cs
+++ Backend/Backend/Startup.cs
@@ -2,14 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Backend.DBContext;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Newtonsoft.Json.Serialization;
 
 namespace Backend
 {
@@ -25,7 +28,16 @@ namespace Backend
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1).AddJsonOptions(options =>
+            {
+                var Resolver = options.SerializerSettings.ContractResolver;
+                if(Resolver != null)
+                {
+                    (Resolver as DefaultContractResolver).NamingStrategy = null;
+                }
+            });
+            services.AddDbContext<Crud_Context>(options => options.UseSqlServer(Configuration["Connectionstrings: getDBConnection"]));
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +53,7 @@ namespace Backend
             }
 
             app.UseHttpsRedirection();
+            app.UseCors(options => options.WithOrigins("http://localhost:4200").AllowAnyHeader().AllowAnyMethod());
             app.UseMvc();
         }
     }
